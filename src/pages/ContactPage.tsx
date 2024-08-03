c1f7447cbed0b75ca8e340346dbce7a50dc33879
Ildar <ildar.1795@gmail.com>
2024-08-03 17:25:52 +0400

    refacor: заменяет локальный на стейт из redux

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,27 +1,23 @@
-import React, {FC, useEffect, useState} from 'react';
-import {CommonPageProps} from './types';
-import {Col, Row} from 'react-bootstrap';
-import {useParams} from 'react-router-dom';
-import {ContactDto} from 'src/types/dto/ContactDto';
-import {ContactCard} from 'src/components/ContactCard';
-import {Empty} from 'src/components/Empty';
+import React, { FC, useEffect, useState } from 'react'
+import { CommonPageProps } from './types'
+import { Col, Row } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { ContactDto } from 'src/types/dto/ContactDto'
+import { ContactCard } from 'src/components/ContactCard'
+import { Empty } from 'src/components/Empty'
+import { useSelector } from 'react-redux'
+import { useAppSelector } from 'src/redux/hooks'
 
+export const ContactPage: FC = () => {
+	const { contactId } = useParams<{ contactId: string }>()
+	const contacts = useAppSelector(state => state.contacts)
+	const contact = contacts.find(({ id }: { id: string }) => id === contactId)
 
-export const ContactPage: FC<CommonPageProps> = ({
-  contactsState
-}) => {
-  const {contactId} = useParams<{ contactId: string }>();
-  const [contact, setContact] = useState<ContactDto>();
-
-  useEffect(() => {
-    setContact(() => contactsState[0].find(({id}) => id === contactId));
-  }, [contactId]);
-
-  return (
-    <Row xxl={3}>
-      <Col className={'mx-auto'}>
-        {contact ? <ContactCard contact={contact} /> : <Empty />}
-      </Col>
-    </Row>
-  );
-};
+	return (
+		<Row xxl={3}>
+			<Col className={'mx-auto'}>
+				{contact ? <ContactCard contact={contact} /> : <Empty />}
+			</Col>
+		</Row>
+	)
+}
