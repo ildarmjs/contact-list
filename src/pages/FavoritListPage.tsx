a5f530f9e6333c86163ab513616c699512f6d93c
Ildar <ildar.1795@gmail.com>
2024-08-09 18:47:40 +0400

    refactor: замена получения данных через RTK query

--- src/pages/FavoritListPage.tsx
+++ src/pages/FavoritListPage.tsx
@@ -1,22 +1,25 @@
 import { memo } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import { ContactCard } from 'src/components/ContactCard'
+import { useGetContactsQuery } from 'src/redux/contacts-api'
 import { useAppSelector } from 'src/redux/hooks'
 
 export const FavoritListPage = memo(() => {
 	const favoriteContacts = useAppSelector(state => state.favorites)
-	const contactsState = useAppSelector(state => state.contacts)
+	const { data: contactsState } = useGetContactsQuery()
 
-	const contacts = contactsState.filter(({ id }: { id: any }) =>
+	const contacts = contactsState?.filter(({ id }: { id: any }) =>
 		favoriteContacts.includes(id)
 	)
+
 	return (
 		<Row xxl={4} className='g-4'>
-			{contacts.map((contact: any) => (
-				<Col key={contact.id}>
-					<ContactCard contact={contact} withLink />
-				</Col>
-			))}
+			{contacts &&
+				contacts.map((contact: any) => (
+					<Col key={contact.id}>
+						<ContactCard contact={contact} withLink />
+					</Col>
+				))}
 		</Row>
 	)
 })
--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -1,22 +1,19 @@
-import React, { memo, useEffect, useState } from 'react'
-import { CommonPageProps } from './types'
+import { memo } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
-import { ContactDto } from 'src/types/dto/ContactDto'
-import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
 import { GroupContactsCard } from 'src/components/GroupContactsCard'
 import { Empty } from 'src/components/Empty'
 import { ContactCard } from 'src/components/ContactCard'
-import { useSelector } from 'react-redux'
-import { useAppSelector } from 'src/redux/hooks'
+import { useGetGroupsQuery } from 'src/redux/groups-api'
+import { useGetContactsQuery } from 'src/redux/contacts-api'
 
 export const GroupPage = memo(() => {
 	const { groupId } = useParams<{ groupId: string }>()
-	const groupContactsState = useAppSelector(state => state.groups)
-	const contactsState = useAppSelector(state => state.contacts)
-	const findGroup = groupContactsState.find(({ id }) => id === groupId)
+	const { data: groupContactsState } = useGetGroupsQuery()
+	const { data: contactsState } = useGetContactsQuery()
+	const findGroup = groupContactsState?.find(({ id }) => id === groupId)
 	const contacts = findGroup
-		? contactsState.filter(({ id }) => findGroup.contactIds.includes(id))
+		? contactsState?.filter(({ id }) => findGroup.contactIds.includes(id))
 		: []
 
 	return (
@@ -32,7 +29,7 @@ export const GroupPage = memo(() => {
 					</Col>
 					<Col>
 						<Row xxl={4} className='g-4'>
-							{contacts.map(contact => (
+							{contacts?.map(contact => (
 								<Col key={contact.id}>
 									<ContactCard contact={contact} withLink />
 								</Col>
